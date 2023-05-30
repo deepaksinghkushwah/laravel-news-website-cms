9c1d861d69643616228e0b9bc4713f38335f924e
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2023-05-30 14:01:17 +0530

    updates

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -51,7 +51,7 @@
   <div class="container-fluid p-0">
     <!-- logo -->
     <a class="navbar-brand" href="index.html">
-      <img src="images/logo.png" alt="logo">
+      <img src="{{ asset('/frontend-theme/images/logo.png') }}" alt="logo">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -59,43 +59,13 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav mx-auto">
-        <li class="nav-item dropdown active">
-          <a class="nav-link" href="#!" data-toggle="dropdown">Home <i class="fa fa-angle-down"></i>
-            <span>/</span>
-          </a>
-          <!-- Dropdown list -->
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="index.html">Homepage</a></li>
-            <li><a class="dropdown-item" href="homepage-two.html">Homepage 2</a></li>
-          </ul>
+      <li class="nav-item">
+          <a class="nav-link" href="{{ route('category.index') }}">News<span>/</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="speakers.html">Speakers
             <span>/</span>
           </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="#!" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i><span>/</span></a>
-          <!-- Dropdown list -->
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="about-us.html">About Us</a></li>
-            <li><a class="dropdown-item" href="single-speaker.html">Single Speaker</a></li>
-            <li><a class="dropdown-item" href="gallery.html">Gallery</a></li>
-            <li><a class="dropdown-item" href="gallery-two.html">Gallery-02</a></li>
-            <li><a class="dropdown-item" href="testimonial.html">Testimonial</a></li>
-            <li><a class="dropdown-item" href="pricing.html">Pricing</a></li>
-            <li><a class="dropdown-item" href="FAQ.html">FAQ</a></li>
-            <li><a class="dropdown-item" href="404.html">404</a></li>
-
-            <li class="dropdown dropdown-submenu dropright">
-              <a class="dropdown-item dropdown-toggle" href="#!" id="dropdown0301" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sub Menu</a>
-
-              <ul class="dropdown-menu" aria-labelledby="dropdown0301">
-                <li><a class="dropdown-item" href="index.html">Submenu 01</a></li>
-                <li><a class="dropdown-item" href="index.html">Submenu 02</a></li>
-              </ul>
-            </li>
-          </ul>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="schedule.html">Schedule<span>/</span></a>
@@ -103,15 +73,13 @@
         <li class="nav-item">
           <a class="nav-link" href="sponsors.html">Sponsors<span>/</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('category.index') }}">News<span>/</span></a>
-        </li>
+
         <li class="nav-item">
           <a class="nav-link" href="contact.html">Contact</a>
         </li>
       </ul>
       <a href="contact.html" class="ticket">
-        <img src="images/icon/ticket.png" alt="ticket">
+        <img src="{{ asset('/frontend-theme/images/icon/ticket.png') }}" alt="ticket">
         <span>Buy Ticket</span>
       </a>
     </div>
@@ -250,7 +218,7 @@
       <div class="col-md-12">
         <div class="block text-center">
           <div class="footer-logo">
-            <img src="images/footer-logo.png" alt="logo" class="img-fluid">
+            <img src="{{ asset('/frontend-theme/images/footer-logo.png') }}" alt="logo" class="img-fluid">
           </div>
           <ul class="social-links-footer list-inline">
             <li class="list-inline-item">
